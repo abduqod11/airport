07886cea7bc51865d3908399b00562da06ee4d5f
Abduqodir <abduqodirxojiakbarov202gmail.com>
2024-03-01 22:48:08 +0500

    add abu

--- Airport/Airport.java
+++ Airport/Airport.java
@@ -13,8 +13,8 @@ public class Airport {
     static Scanner strScanner = new Scanner(System.in);
     static Scanner intScanner = new Scanner(System.in);
     static Scanner str2Scanner = new Scanner(System.in);
-    static String[] log = {"qwerty@gmai l.com","asdfg112@gamil.com","erghs34@gamil.com"};
-    static String[] password = {"qwe123","esd0929","ersim8103"};
+    static String[] log = {"qwerty@gmail.com", "asdfg112@gamil.com", "erghs34@gamil.com"};
+    static String[] password = {"qwe123", "esd0929", "ersim8103"};
 
     static String[] regions = {"Toshkent", "Toshkent viloyati", "Andijon viloyati", "Farg'ona viloyati", "Namangan viloyati", "Sirdaryo viloyati",
             "Jizzax viloyati", "Samarqand viloyati", "Surxandaryo viloyati", "Qashqadaryo viloyati", "Buxoro viloyati", "Navoiy viloyati",
@@ -25,18 +25,19 @@ public class Airport {
         String loginAcc = strScanner.next();
         System.out.print("Parolni kiriting: ");
         String passwordMenu = strScanner.next();
+        boolean bool = false;
         for (int i = 0; i < log.length; i++) {
             if (loginAcc.equals(log[i]) && passwordMenu.equals(password[i])) {
-
+                bool = true;
                 getreaces();
                 System.out.println("                                                              ✈️Xush kelibsiz✈️");
                 System.out.println("                           Siz O'zbekiston havo yoʻllari \uD83C\uDF10onlayn saytiga tashrif buyurganingizdan bag'oyatda xursandmiz❗");
                 while (true) {
                     System.out.println("""
                             0. Chiqish
-                            1. Barcha reyslar ko'rish
-                            2. Chipta olish🎫
-                            3. Biz bilan bog'lanish☎️ 
+                            1. Barcha reyslar ko'rish 
+                            2. Chipta olish🎫 
+                            3. Biz bilan bog'lanish☎️
                             """);
                     int tanla = intScanner.nextInt();
                     if (tanla == 0) break;
@@ -47,6 +48,9 @@ public class Airport {
                     }
                 }
             }
+            if (bool == false)
+                System.out.println("Email yoki parolda xatolik mavjud,iltimos qayta tekshirib kiriting!");
+            break;
         }
     }
 
@@ -73,8 +77,7 @@ public class Airport {
                             oy++;
                         }
                     }
-                }
-                else{
+                } else {
                     if (i != j) {
                         int price = (int) (Math.random() * 200 + 50);
                         Line line;
@@ -115,16 +118,16 @@ public class Airport {
         }
     }
 
-    private static void checkOut(){
+    private static void checkOut() {
         boolean check = false;
-        while (true){
+        while (true) {
             System.out.println("""
                     0. Chiqish
                     1. Chipta sotib olish🎫
                     """);
             int tanla = intScanner.nextInt();
-            if(tanla == 0) break;
-            if(tanla == 1){
+            if (tanla == 0) break;
+            if (tanla == 1) {
                 System.out.print("Ismingizni kiriting: ");
                 String firstname = strScanner.next();
                 System.out.print("Familiyangizni kiriting: ");
@@ -136,24 +139,24 @@ public class Airport {
                 System.out.print("Qaysi viloyatga bormoqchisiz: ");
                 String destination = str2Scanner.nextLine();
                 for (int i = 0; i < lines.size(); i++) {
-                    if((Objects.equals(lines.get(i).getOrigin().toLowerCase(),origin.toLowerCase())) &&
-                            (Objects.equals(lines.get(i).getDestination().toLowerCase(),destination.toLowerCase()))){
+                    if ((Objects.equals(lines.get(i).getOrigin().toLowerCase(), origin.toLowerCase())) &&
+                            (Objects.equals(lines.get(i).getDestination().toLowerCase(), destination.toLowerCase()))) {
 
                         System.out.println("------------------------------\n" +
-                                          "Ismingiz: " + firstname + "\n" +
-                                          "Familiyangiz: " + surname + "\n" +
-                                          "Yoshingiz: " + (2023 - year) + "\n" +
-                                          "Qayerdan: " + origin + "\n" +
-                                          "qayerga: " + destination + "\n" +
-                                          "Ketish: " + lines.get(i).getDeparture() + "\n" +
-                                          "Chipta narxi: " + lines.get(i).getPrice() + "$\n------------------------------");
+                                "Ismingiz: " + firstname + "\n" +
+                                "Familiyangiz: " + surname + "\n" +
+                                "Yoshingiz: " + (2023 - year) + "\n" +
+                                "Qayerdan: " + origin + "\n" +
+                                "qayerga: " + destination + "\n" +
+                                "Ketish: " + lines.get(i).getDeparture() + "\n" +
+                                "Chipta narxi: " + lines.get(i).getPrice() + "$\n------------------------------");
                         check = true;
                         break;
                     }
                 }
-                if(!check){
+                if (!check) {
                     System.out.println("Malumotni tog'ri kiriting");
-                }else{
+                } else {
                     break;
                 }
             }
@@ -171,7 +174,7 @@ public class Airport {
     }
 
     private static void oneRegion() {
-        while(true){
+        while (true) {
             boolean check = false;
             System.out.println("""
                     0. Chiqish
@@ -179,11 +182,11 @@ public class Airport {
                     """);
             int tanla = intScanner.nextInt();
             if (tanla == 0) break;
-            if(tanla == 1){
+            if (tanla == 1) {
                 System.out.print("Shahar kiriting: ");
-                String place = strScanner.nextLine();
-                for (int i = 0; i < lines.size(); i++) {
-                    if (Objects.equals(lines.get(i).getOrigin().toLowerCase(),place.toLowerCase())) {
+                String place = (new Scanner(System.in)).nextLine();
+                 for (int i = 0; i < lines.size(); i++) {
+                    if (Objects.equals(lines.get(i).getOrigin().toLowerCase(), place.toLowerCase())) {
                         check = true;
                         String row1 = lines.get(i).getOrigin();
                         String row2 = lines.get(i).getDestination();
@@ -192,54 +195,55 @@ public class Airport {
                         System.out.printf("%-33s %-33s %-15s %-6s%n", row1, row2, date1, date2);
                     }
                 }
-                if (!check) System.out.println("Bunday viloyat mavjud emas");
+//                    if (!check) System.out.println("Bunday viloyat mavjud emas");
             }
-            if(check) break;
-        }
-    }
-    private static void searchDate(){
-        boolean check = false;
-        while (true){
-            System.out.println("""
-                0. Chiqish
-                1. Sana kiritish
-                """);
-            int tanla = intScanner.nextInt();
-            if(tanla == 0) break;
-            if(tanla == 1){
-                System.out.print("Sana kiriting: ");
-                String date = strScanner.next();
-                for(int i = 0; i < lines.size(); i++){
-                    if(Objects.equals(lines.get(i).getDeparture(),date)){
-                        check = true;
-                        String row1 = lines.get(i).getOrigin();
-                        String row2 = lines.get(i).getDestination();
-                        String date1 = lines.get(i).getDeparture();
-                        String date2 = lines.get(i).getWreturn();
-                        System.out.printf("%-33s %-33s %-15s %-6s%n", row1, row2, date1, date2);
-                    }
-                }
-                if(!check) System.out.println("Bunday sanadagi reys mavjud emas");
-            }
-            if(check) break;
+            if (check) break;
         }
     }
 
-    private static void lineWithMe(){
-        while (true){
+    private static void searchDate() {
+        boolean check = false;
+        while (true) {
+            System.out.println("""
+                    0. Chiqish
+                    1. Sana kiritish
+                    """);
+            int tanla = intScanner.nextInt();
+            if (tanla == 0) break;
+            if (tanla == 1) {
+                System.out.print("Sana kiriting: ");
+                String date = strScanner.next();
+                for (int i = 0; i < lines.size(); i++) {
+                    if (Objects.equals(lines.get(i).getDeparture(), date)) {
+                        check = true;
+                        String row1 = lines.get(i).getOrigin();
+                        String row2 = lines.get(i).getDestination();
+                        String date1 = lines.get(i).getDeparture();
+                        String date2 = lines.get(i).getWreturn();
+                        System.out.printf("%-33s %-33s %-15s %-6s%n", row1, row2, date1, date2);
+                    }
+                }
+                if (!check) System.out.println("Bunday sanadagi reys mavjud emas");
+            }
+            if (check) break;
+        }
+    }
+
+    private static void lineWithMe() {
+        while (true) {
             System.out.print("Nomer kiriting: ");
             long number = intScanner.nextLong();
-            if(number >= 10){
+            if (number >= 10) {
                 System.out.println("Sizning telefon raqamingizga sms kod yuborildi");
-                int sentCode = (int)(Math.random() * 10000);
+                int sentCode = (int) (Math.random() * 10000);
                 System.out.println("Sizning kodingiz: " + sentCode);
                 System.out.print("Sms kodingizni kiriting: ");
                 int enterCode = intScanner.nextInt();
-                if(enterCode == sentCode){
+                if (enterCode == sentCode) {
                     System.out.println("Siz bilan qisqa vaqtlarda aloqaga chiqamiz");
-                }else System.out.println("Sms kodini xato kiritingiz, tekshirib qayta kiriting");
-            }else System.out.println("Telefon raqami xato kiritildi");break;
-
+                } else System.out.println("Sms kodini xato kiritingiz, tekshirib qayta kiriting");
+            } else System.out.println("Telefon raqami xato kiritildi");
+            break;
         }
     }
 }
